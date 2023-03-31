af9a1e6515cfb1835af81d53f6eb953d03853b95
ishaan <67547661+ishaan199@users.noreply.github.com>
2023-03-31 22:52:55 +0530

    adding api and make website dynamic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
-
+import {useState} from 'react';
 
 function Home() {
+    const [searchStr, setSearchStr] = useState("");
+
+
+    const onSearchInputChange = (ev) => {
+        setSearchStr(ev.target.value);
+    }
+
+    const onSearch = async (ev) => {
+        ev.preventDefault();
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`)
+        const body = await response.json();
+        console.log(body);
+    }
+
   return (
     <div>
-      <h1>home</h1>
+        <form onSubmit={onSearch}>
+            <input type="text" name="searchInput" value ={searchStr.searchInput} onChange={onSearchInputChange}/>
+            <button type="submit">Search</button>
+        </form>
+      
     </div>
   )
 }
